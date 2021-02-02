98975ace9777ba028c4b52aa71da62bb17902b96
canadmin <icanyard@gmail.com>
2021-02-02 22:52:34 +0300

    bug fixed

--- src/common/socket.js
+++ src/common/socket.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import store from "../store/store";
 
-const socket = new WebSocket("ws://https://dualchat2.herokuapp.com/socket");
+const socket = new WebSocket("wss://dualchat2.herokuapp.com/socket");
 
 const emitter = new Vue({
   methods:{
