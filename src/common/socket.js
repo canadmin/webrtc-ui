d18aa5e37723842fb13cfc2c0eecfb8f57e86f12
canadmin <icanyard@gmail.com>
2021-02-03 21:41:46 +0300

    heroku

--- src/common/socket.js
+++ src/common/socket.js
@@ -1,8 +1,8 @@
 import Vue from "vue";
 import store from "../store/store";
 
-//const socket = new WebSocket("wss://dualchat2.herokuapp.com/socket");
-const socket = new WebSocket("ws://localhost:8081/socket");
+const socket = new WebSocket("wss://dualchat2.herokuapp.com/socket");
+//const socket = new WebSocket("ws://localhost:8081/socket");
 
 
 const emitter = new Vue({
